75950b6bb82ab965da6c14373b5e47bcb9257036
loay fayez <84818419+leon019i@users.noreply.github.com>
2023-04-10 23:16:31 +0200

    Fixing the slider routes and some files format

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { PageMainComponent } from './pages/page-management/page-main/page-main.component';
-import { AppComponent } from './app.component';
-import { LandingContentComponent } from './pages/landing-page/landing-content/landing-content.component';
-import { LandingMainComponent } from './pages/landing-page/landing-main/landing-main.component';
-import { PageEditComponent } from './pages/page-management/page-edit/page-edit.component';
-import { UserMainComponent } from './pages/user-management/user-main/user-main.component';
-import { RoleMainComponent } from './pages/role-management/role-main/role-main.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {PageMainComponent} from './pages/page-management/page-main/page-main.component';
+import {AppComponent} from './app.component';
+import {LandingContentComponent} from './pages/landing-page/landing-content/landing-content.component';
+import {LandingMainComponent} from './pages/landing-page/landing-main/landing-main.component';
+import {PageEditComponent} from './pages/page-management/page-edit/page-edit.component';
+import {UserMainComponent} from './pages/user-management/user-main/user-main.component';
+import {RoleMainComponent} from './pages/role-management/role-main/role-main.component';
+import {ClassBuilderMainComponent} from "./pages/class-builder/class-builder-main/class-builder-main.component";
 
 const routes: Routes = [
-  { path: '', component: LandingMainComponent},
-  {path:'PageManagement',component:PageMainComponent},
-  {path:'EditPage',component:PageEditComponent},
-  {path:'UserManagement',component:UserMainComponent},
-  {path:'RoleManagement',component:RoleMainComponent}
+  {path: '', component: LandingMainComponent},
+  {path: 'PageManagement', component: PageMainComponent},
+  {path: 'EditPage', component: PageEditComponent},
+  {path: 'UserManagement', component: UserMainComponent},
+  {path: 'ClassBuilder', component: ClassBuilderMainComponent},
+  {path: 'RoleManagement', component: RoleMainComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
